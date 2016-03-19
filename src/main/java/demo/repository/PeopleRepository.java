42a1d1e2ddcd9e56193c60256f6c09ae7db6b710
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2016-03-19 14:23:21 -0700

    added user

--- src/main/java/demo/repository/PeopleRepository.java
+++ src/main/java/demo/repository/PeopleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-
+import cpabe.*;
 import java.util.List;
 
 @RepositoryRestResource(path = "people", collectionResourceRel = "person", itemResourceRel = "person")
