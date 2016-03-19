d52f33b604dd7491aa1ba914d32cee404231f5ed
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2016-03-19 14:18:39 -0700

    added user

---
+++ src/main/java/demo/model/User.java
@@ -0,0 +1,62 @@
+package demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User implements Serializable
+{
+   /** Default value included to remove warning. Remove or modify at will. **/
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   @GeneratedValue
+   private Long id;
+
+   @NotNull
+   private String password;
+
+   @NotNull
+   @NotEmpty
+   @Email
+   private String email;
+
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getpassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+}
